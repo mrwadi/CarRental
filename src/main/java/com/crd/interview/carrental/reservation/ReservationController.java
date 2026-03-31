8e1914a89aa840b77bdb7be8e5e356d4be086e15
waldek <waldek.serafin@gmail.com>
2026-03-31 20:10:38 +0200

    add endpoint + log jpa queries

---
+++ src/main/java/com/crd/interview/carrental/reservation/ReservationController.java
@@ -0,0 +1,25 @@
+package com.crd.interview.carrental.reservation;
+
+import com.crd.interview.carrental.reservation.dto.ReservationRequest;
+import com.crd.interview.carrental.reservation.dto.ReservationResponse;
+import com.crd.interview.carrental.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping
+    public ResponseEntity<ReservationResponse> create(@RequestBody ReservationRequest request) {
+        ReservationResponse result = reservationService.createReservation(request);
+        return ResponseEntity.ok(result);
+    }
+}
