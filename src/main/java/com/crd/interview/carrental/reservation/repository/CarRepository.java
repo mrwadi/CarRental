fc717f93faf1bfee9c64d133ea224f5553cce83b
waldek <waldek.serafin@gmail.com>
2026-03-31 19:54:23 +0200

    workaround compilation error

--- src/main/java/com/crd/interview/carrental/reservation/repository/CarRepository.java
+++ src/main/java/com/crd/interview/carrental/reservation/repository/CarRepository.java
@@ -3,11 +3,13 @@ package com.crd.interview.carrental.reservation.repository;
 import com.crd.interview.carrental.reservation.model.Car;
 import com.crd.interview.carrental.reservation.model.CarType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<com.crd.interview.carrental.reservation.model.Car,Long> {
-    //TODO add query to find available car , join with reservations
+    //FIXME fix query to find available car , join with reservations
+    @Query("SELECT c FROM Car c ORDER BY c.id LIMIT 1")
     Optional<Car> findAvailableCar(CarType carType, LocalDateTime reservationStartDate, LocalDateTime reservationEndDate);
 }
