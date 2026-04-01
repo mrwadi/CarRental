e821d53091505f13ff0beb77a7abdb181f2b1320
waldek <waldek.serafin@gmail.com>
2026-04-01 22:41:30 +0200

    schedule job to cleanup db

---
+++ src/main/java/com/crd/interview/carrental/reservation/job/OutDatedReservationDBCleaner.java
@@ -0,0 +1,21 @@
+package com.crd.interview.carrental.reservation.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OutDatedReservationDBCleaner {
+
+//    private final ReservationRepository reservationRepository;
+
+    @Scheduled(cron = "0 * * * * *") // Co minutę
+    @Transactional
+    public void cleanOutdatedReservations() {
+        log.info("Delete items form db job triggered");
+    }
+}
